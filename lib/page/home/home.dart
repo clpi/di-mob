72e2ae1b97ceed950730c7f5301f4bad2efc578b
clpi <chrisp1877@gmail.com>
2021-02-13 01:04:09 -0800

    cleaning home

--- lib/page/home/home.dart
+++ lib/page/home/home.dart
@@ -59,51 +59,7 @@ class _HomePageState extends State<HomePage>
       floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
       drawer: dlDrawer,
       appBar: DlAppBar(key: Key("appBar"), title: "devisa").bar(context),
-      // appBar: AppBar(
-      //   automaticallyImplyLeading: true,
-      //   centerTitle: true,
-      //   elevation: 10.0,
-      //   title: Row(children: [
-      //     Text("devisa", style: TextStyle(color: Colors.white, fontSize: 21.0),),
-      //   ],
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //   ),
-      //   backgroundColor: Colors.deepPurpleAccent,
-      //   actions: [
-      //     // IconButton(icon: Icon(Icons.list_alt), onPressed: () => _viewRecords(context),),
-      //     IconButton(
-      //       icon: Icon(Icons.my_library_add_rounded),
-      //       onPressed: () => DlListSheet().show(context),
-      //     ),
-      //   ],
-      // ),
       body: body(context),
-      // body: SingleChildScrollView(
-      //   child: SafeArea(
-      //     child: Container(
-      //       padding: EdgeInsets.symmetric(horizontal: 16.0),
-      //       child: Column(
-      //         crossAxisAlignment: CrossAxisAlignment.stretch,
-      //         mainAxisSize: MainAxisSize.min,
-      //         children: [
-      //           SizedBox(
-      //             height: 20.0,
-      //           ),
-      //           RichText(
-      //             text: TextSpan(
-      //               children: [
-      //                 TextSpan(
-      //                   text: "Home",
-      //                   style: TextStyle(
-      //                     fontSize: 22.0,
-      //                     height: 1.3,
-      //                     fontWeight: FontWeight.w300,
-      //                     color: Colors.white,
-      //                   ),
-      //                 ),
-      //               ],
-      //             ),
-      //           ),],),),),),
       bottomNavigationBar: DlBottomBar(restorationId: "", key: Key(""), type: BottomBarKind.Labels),
       endDrawerEnableOpenDragGesture: true,
       resizeToAvoidBottomInset: true,
